308c2257c6e3fc2346ef7f6db5794e10a226bbcd
Kea Maremela <keamogetsemaremela29@gmail.com.>
2026-07-13 11:01:59 +0200

    commiting series dictionaries

--- series_dictionary.py
+++ series_dictionary.py
@@ -1,3 +1,17 @@
 import pandas as pd
 
 calories = {"day1": 420,"day2": 380, "day3": 390, "day4": 400, "day5": 450}
+
+series = pd.Series(calories)
+print(f"printing the series:\n{series}")
+
+#Number of calories on day 1
+print(f"Number of calories on day 1: {series['day1']}")
+
+# updating the value of calories on day 3
+series['day3']= 500
+
+
+# adding calories for day 5
+series['day5'] += 60
+print(f"Adding calories for day 5 from 450 to 510:\n{series}")
