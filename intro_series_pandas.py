0a67240f2f73ab78ffdc9aaf245b3d551ac8e4ca
Kea Maremela <keamogetsemaremela29@gmail.com.>
2026-07-13 10:35:23 +0200

    Series dictionarie in pandas

--- intro_series_pandas.py
+++ intro_series_pandas.py
@@ -1,16 +1,17 @@
 import pandas as pd
 
-data = [100,103, 104]
+data = [100,103, 104, 105, 106]
 
-series = pd.Series(data, index= ["a","b" , "c"])
-print(f"printing the series:\n{series}")
+series = pd.Series(data, index= ["a","b","c", "d", "e"])
 
-# LOCATING A VLUE AT A VERTAIN LABLE
-print(f"Locating a value at lable a: {series.loc["a"]}")
-print(f"Locating a value at lable b: {series.loc["b"]}")
-print(f"Locating a value at lable c: {series.loc["c"]}")
+print(f"printing values greater than or equal to 103:\n{series[series>=103]}")
 
-# INTEGER LOCATION
-print(f"printing integer location index 0: {series.iloc[0]}")
-print(f"printing integer location index 1: {series.iloc[1]}")
-print(f"printing integer location index 2: {series.iloc[2]}")
+# # LOCATING A VLUE AT A VERTAIN LABLE
+# print(f"Locating a value at lable a: {series.loc["a"]}")
+# print(f"Locating a value at lable b: {series.loc["b"]}")
+# print(f"Locating a value at lable c: {series.loc["c"]}")
+
+# # INTEGER LOCATION
+# print(f"printing integer location index 0: {series.iloc[0]}")
+# print(f"printing integer location index 1: {series.iloc[1]}")
+# print(f"printing integer location index 2: {series.iloc[2]}")
---
+++ series_dictionary.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+calories = {"day1": 420,"day2": 380, "day3": 390, "day4": 400, "day5": 450}
